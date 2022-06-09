1d7b51b2d5246d958f29f91ba0150317e26bb0d9
DeepBlueDevOps <92817968+DeepBlueDevOps@users.noreply.github.com>
2022-06-08 18:38:07 -0700

    Initial baseline 1.5 changes

--- screens/GetCounts/styles.js
+++ screens/GetCounts/styles.js
@@ -1,96 +1,91 @@
-import { StyleSheet } from 'react-native';
-
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    origTitle: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginTop: 50,
-        padding: 20,
-    },
-    container: {
-        flex: 1,
-        //marginTop: StatusBar.currentHeight || 0,
-    },
-    item: {
-        padding: 20,
-        marginVertical: 8,
-        marginHorizontal: 16,
-    },
-    title: {
-        fontSize: 32,
-    },
-    container: {
-        backgroundColor: '#f4f4f4',
-        flex: 1,
-    },
-    backTextWhite: {
-        color: '#FFF',
-    },
-    rowFront: {
-        backgroundColor: '#FFF',
-        borderRadius: 5,
-        height: 60,
-        margin: 5,
-        marginBottom: 15,
-        shadowColor: '#999',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        elevation: 5,
-    },
-    rowFrontVisible: {
-        backgroundColor: '#FFF',
-        borderRadius: 5,
-        height: 60,
-        padding: 10,
-        marginBottom: 15,
-    },
-    rowBack: {
-        alignItems: 'center',
-        backgroundColor: '#DDD',
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingLeft: 15,
-        margin: 5,
-        marginBottom: 15,
-        borderRadius: 5,
-    },
-    backRightBtn: {
-        alignItems: 'flex-end',
-        bottom: 0,
-        justifyContent: 'center',
-        position: 'absolute',
-        top: 0,
-        width: 75,
-        paddingRight: 17,
-    },
-    backRightBtnLeft: {
-        backgroundColor: '#1f65ff',
-        right: 75,
-    },
-    backRightBtnRight: {
-        backgroundColor: 'red',
-        right: 0,
-        borderTopRightRadius: 5,
-        borderBottomRightRadius: 5,
-    },
-    trash: {
-        height: 25,
-        width: 25,
-        marginRight: 7,
-    },
-    title: {
-        fontSize: 14,
-        fontWeight: 'bold',
-        marginBottom: 5,
-        color: '#666',
-    },
-    details: {
-        fontSize: 12,
-        color: '#999',
-    },
+  origTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 50,
+    padding: 20,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  container: {
+    backgroundColor: '#f4f4f4',
+    flex: 1,
+  },
+  backTextWhite: {
+    color: '#FFF',
+  },
+  rowFront: {
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    height: 60,
+    margin: 5,
+    marginBottom: 15,
+    shadowColor: '#999',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  rowFrontVisible: {
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    height: 60,
+    padding: 10,
+    marginBottom: 15,
+  },
+  rowBack: {
+    alignItems: 'center',
+    backgroundColor: '#DDD',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 15,
+    margin: 5,
+    marginBottom: 15,
+    borderRadius: 5,
+  },
+  backRightBtn: {
+    alignItems: 'flex-end',
+    bottom: 0,
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    width: 75,
+    paddingRight: 17,
+  },
+  backRightBtnLeft: {
+    backgroundColor: '#1f65ff',
+    right: 75,
+  },
+  backRightBtnRight: {
+    backgroundColor: 'red',
+    right: 0,
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  trash: {
+    height: 25,
+    width: 25,
+    marginRight: 7,
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    color: '#666',
+  },
+  details: {
+    fontSize: 12,
+    color: '#999',
+  },
 });
 
 export default styles;
--- screens/SaveCount/index.js
+++ screens/SaveCount/index.js
@@ -1,98 +1,101 @@
-/* eslint-disable prettier/prettier */
-import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text} from 'react-native';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useTheme } from '@react-navigation/native';
+import {useTheme} from '@react-navigation/native';
 import styles from './styles';
 
+const SaveCountScreen = ({route, navigation}) => {
+  const [listData, updateListData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const {colors} = useTheme();
 
-const SaveCountScreen = ({ route, navigation }) => {
-    const [listData, updateListData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const { colors } = useTheme();
+  const count = route.params;
+  const dateInfo = getFormatedDate();
+  const key = getKey();
 
-    const count = route.params;
-    const dateInfo = getFormatedDate();
-    const key = getKey();
+  const STORAGEKEY = '@StorageKEY';
 
-    const STORAGEKEY = "@StorageKEY";
+  useEffect(() => {
+    getData();
+  }, []);
+  //https://reactjs.org/docs/hooks-rules.html
+  //https://dev.to/spukas/4-ways-to-useeffect-pf6
+  //useEffect called when component Mounts
 
-    useEffect(() => { getData(); }, []);
-    //https://reactjs.org/docs/hooks-rules.html
-    //https://dev.to/spukas/4-ways-to-useeffect-pf6
-    //useEffect called when component Mounts
+  const getData = async () => {
+    const resp = await AsyncStorage.getItem(STORAGEKEY);
+    const data = await JSON.parse(resp);
+    updateListData(data);
+    setLoading(false);
+    console.log('getData completed');
+  };
 
+  useEffect(() => {
+    storeData();
+  }, [loading]);
 
-    const getData = async () => {
-        const resp = await AsyncStorage.getItem(STORAGEKEY);
-        const data = await JSON.parse(resp);
-        updateListData(data);
-        setLoading(false);
-        console.log('getData completed');
+  function getKey() {
+    const d = new Date();
+
+    let time = d.getTime();
+    let date = d.getFullYear() + d.getMonth() + d.getDay();
+    let formatedKey = date + time;
+
+    return formatedKey;
+  }
+
+  function getFormatedDate() {
+    const d = new Date();
+
+    let options = {
+      weekday: 'short',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
     };
 
-    useEffect(() => { storeData(); }, [loading]);
+    let fullDateTime = new Intl.DateTimeFormat('en-US', options).format(d);
+    return fullDateTime;
+  }
 
-    function getKey() {
+  const storeData = async () => {
+    //let numKeys = listData.length + 1;
+    let newListElement = {key: key, count: count, countDate: dateInfo};
+    let newListItem = [newListElement];
+    let newListData = [...listData, ...newListItem];
 
-        const d = new Date();
+    updateListData(newListData);
 
-        let time = d.getTime();
-        let date = d.getFullYear() + d.getMonth() + d.getDay();
-        let formatedKey = date + time;
-
-        return formatedKey;
+    console.log(JSON.stringify(newListItem));
+    console.log(JSON.stringify(newListData));
+    try {
+      await AsyncStorage.setItem(STORAGEKEY, JSON.stringify(newListData));
+    } catch (error) {
+      console.log('Error:' + error);
     }
+    console.log('Async called');
+  };
 
-    function getFormatedDate() {
+  return (
+    <View>
+      <Text style={[styles.title, {color: colors.text}]}>Details Saved</Text>
 
-        const d = new Date();
-
-        let options = {
-            weekday: 'short', year: 'numeric', month: 'short', day: 'numeric',
-            hour: '2-digit', minute: '2-digit', second: '2-digit',
-        };
-
-        let fullDateTime = new Intl.DateTimeFormat("en-US", options).format(d);
-        return fullDateTime;
-    }
-
-    const storeData = async () => {
-
-        //let numKeys = listData.length + 1;
-        let newListElement = { key: key, count: count, countDate: dateInfo };
-        let newListItem = [newListElement];
-        let newListData = [...listData, ...newListItem];
-
-        updateListData(newListData);
-
-        console.log(JSON.stringify(newListItem));
-        console.log(JSON.stringify(newListData));
-        try {
-            await AsyncStorage.setItem(STORAGEKEY, JSON.stringify(newListData));
-        } catch (error) {
-            console.log('Error:' + error);;
-        };
-        console.log('Async called');
-    }
-
-
-
-    return (
-        <View>
-            <Text style={[styles.title, { color: colors.text }]}>Details Saved</Text>
-
-            <View style={styles.row}>
-                <Text style={[styles.titleCell, { color: colors.text }]}>Count:</Text>
-                <Text style={{ marginLeft: 60, color: colors.text }}> {count} </Text>
-            </View>
-            <View style={styles.row}>
-                <Text style={[styles.titleCell, { color: colors.text }]}>Date, Time:</Text>
-                <Text style={{ marginLeft: 30, color: colors.text }}>{dateInfo}</Text>
-            </View>
-        </View>
-    );
+      <View style={styles.row}>
+        <Text style={[styles.titleCell, {color: colors.text}]}>Count:</Text>
+        <Text style={{marginLeft: 60, color: colors.text}}> {count} </Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={[styles.titleCell, {color: colors.text}]}>
+          Date, Time:
+        </Text>
+        <Text style={{marginLeft: 30, color: colors.text}}>{dateInfo}</Text>
+      </View>
+    </View>
+  );
 };
 
 export default SaveCountScreen;
